41529572c8e6d145449995b4e43b831d0ee6789d
mehjabin hasan <mehjabinhasanmoni@gmail.com>
2024-05-14 11:35:54 +0600

    react-core-concept

--- src/App.js
+++ src/App.js
@@ -1,11 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
 
+
+const number = 555;
+
+const singer = {name : 'Dr. Mahfuz', job : 'Singer'}
+
+const singer2 = { name : 'Eva Rahman', job : 'Singer'}
+
+const singerStyle = {
+  color : 'purple',
+  backgroundColor : '#FFFFFF',
+  padding: '10px',
+
+}
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+       <h1> JSX </h1>
+
+       <div className="container">
+          <h3> Hello Dude, React how are you??</h3>
+       </div>
+
+       <div className="music">
+        <p style={singerStyle}> Name : {number}</p>
+        <p style={{
+  color : 'purple',
+  backgroundColor : 'yellow',
+  padding: '10px',
+
+}}> Singer Name : {singer.name} is a {singer.job} </p>
+       </div> Siger Name : {singer2.name} ,  {singer2.job}
+         
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
